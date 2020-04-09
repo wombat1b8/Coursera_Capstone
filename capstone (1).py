93f8fb831f752bc5ddce6fa75d31221ac6acffe7
wombat1b8 <49267569+wombat1b8@users.noreply.github.com>
2020-04-09 17:15:23 +0800

    Add files via upload

---
+++ capstone (1).py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Capstone Project Notebook ##
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
